08df733fa0ad573ce2f490f0d865c60924039b46
Zenyr <zenyr@zenyr.com>
2014-08-11 22:20:11 +0900

    proceeding ingame migration

--- Hud3.lua
+++ Hud3.lua
@@ -4,8 +4,8 @@ local disclamer = [[
 feel free to ask me through my mail: zenyr@zenyr.com. But please understand that I'm quite clumsy, cannot guarantee I'll reply what you want..
 ]]
 local _ = UNDERSCORE
-local REV = 69
-local TAG = 'v0.111T-19-g2777dd6'
+local REV = 70
+local TAG = 'v0.111T-20-g3711840'
 local inGame = CopDamage ~= nil
 local me
 local function _req(name)
@@ -22,6 +22,8 @@ end
 PocoHud3Class = nil
 _req ('poco/Hud3_class.lua')
 if not PocoHud3Class then return end
+_req ('poco/Hud3_Options.lua')
+local O = PocoHud3Class.Option:new()
 --- Options ---
 local YES,NO,yes,no = true,false,true,false
 local inO = {
@@ -29,118 +31,6 @@ local inO = {
 		color = {'color','White'},
 		size = {'number',22},
 	}
-}
-local O = {
-	enable = YES, -- YES/NO : 포코허드 전체 스위치
-	debug = {
-		color = cl.White:with_alpha(0.5),
-		size = 22,
-		defaultFont = NO,
-		verboseOnly = NO,
-		showFPS = YES,
-		showClockIngame = NO,
-		showClockOutgame = YES,
-	},
-	buff = {			-- === 버프 설정 === >> 재장전, 스태미너, ECM, 버서커, 불렛스톰등 표현
-		show = YES,	-- YES/NO : 버프 표현기능 사용
-		left = 10,  -- 0~100% : 버프 기준점 가로 %
-		top  = 22,  -- 0~100% : 버프 기준점 세로 %
-		maxFPS = 50,-- 자연수 : 버프 갱신주기
-		size = 70,  -- 자연수 : 버프 아이콘 크기 (단, "바닐라 스타일"에서는 무시)
-		gap = 10,   -- 자연수 : 버프 아이콘 간격 (단, "바닐라 스타일"에서는 무시)
-		align = 1,  -- [1,2,3] : 아이콘 정렬방향 1왼쪽 2중앙 3오른쪽
-		style = 2,  -- [1,2]: 버프아이콘 스타일 1포코허드(컬러) 2바닐라(순정Feel)
-
-		noSprintDelay = YES,
-		hideInteractionCircle = NO,
-		simpleBusy = YES,
-		simpleBusyRadius = 10,
-	},
-	popup = {			-- === 데미지팝업 설정 ===
-		show = YES,		-- YES/NO : 데미지팝업 사용
-		size = 20,		-- 자연수 : 팝업 글자크기
-		damageDecay = 10,	-- 초 : 팝업 지속시간
-		myDamage = YES,   	-- YES/NO : 내 데미지 표시
-		crewDamage = YES, 	-- YES/NO : 동료 플레이어 데미지 표시
-		AIDamage = YES,	-- YES/NO : 동료 AI 데미지 표시
-		handsUp = YES,		-- YES/NO : 몹이 투항하려 할 때 표시
-		dominated = YES,	-- YES/NO : 투항 완료시 표시
-	},
-	float = {				-- === 가리킨 대상 정보 설정 ===
-		show = YES,			-- YES/NO : 사용여부
-		border = NO,			-- YES/NO : 모서리 표시 사용
-		size = 15,			-- 자연수 : 정보 글자크기
-		margin = 3,			-- 자연수 : 정보 항목간 간격
-		keepOnScreen = YES,		-- YES/NO : 화면 내 유지여부
-		keepOnScreenMargin = {2,15},	-- {%,%} : 화면 내 유지시 가로, 세로 여백
-		maxOpacity = 0.9,		-- 0-1 : 불투명도
-		unit = YES,			-- YES/NO : 유닛정보 표시
-		drills = YES,			-- YES/NO : 드릴정보 표시
-	},
-	info = {				-- === 플레이어 정보 ===
-		size = 17,			-- 자연수 : 정보 글자크기
-		clock = YES,			-- YES/NO : 내 정보란 빈칸을 활용해 (현실)시간 표시
-		verboseKey = '`',		-- 문자 : 자세히 보기 모드 단축키
-	},
-	minion = {			-- === 미니언 정보 === >> Float을 활용해 Joker스킬 사용한 미니언 표시
-		show = YES		-- YES/NO
-	},
-	chat = {		-- === 주요 이벤트 채팅 방송기능 === >> 이벤트별로 중요도를 할당해, 내 자격에 따라 방송 여부 결정가능
-		readThreshold = 2,			-- 이 수치보다 높은 이벤트 발생시 내 채팅창에만 표시
-		serverSendThreshold = 3,		-- 내가 방장인 경우 플레이어에게 모두 방송
-		clientFullGameSendThreshold = 4,	-- 내가 방장이 아니지만 방장과 동시에 시작했을 경우 플레이어에게 모두 방송
-		clientMidGameSendThreshold = 5,	-- 내가 방장도 아니고 중간에 입장했지만 플레이어에게 모두 방송
-		alwaysSendThreshold = 8, -- Mute상태에서도 무조건 방송
-
-		midgameAnnounce = 50,			-- 팀원+AI합산한 킬수마다 게임 중간 통계 출력 (이벤트 중요도에 따라 방송여부 결정)
-
-		index = { -- Index: 이벤트별 중요도 할당. 높은 수치는 '많은 사람들이 알아야 하는 중요한 정보', 낮은 수치는 '별로 필요 없는 사소한 정보'를 의미
-			midStat = 3,			-- 게임 중간통계 (*게임중 입장시 부정확)
-			endStat = 4,			-- 게임 최종통계 (*게임중 입장시 부정확)
-			endStatCredit = 4, -- 게임 최종통계 후 모드 특성 설명
-			dominated = 4,			-- 몹이 투항했을 경우
-			converted = 4,			-- 플레이어가 몹을 미니언으로 변환한 경우
-			minionLost = 4,		-- 미니언 사망
-			minionShot = 4,		-- 플레이어에 의한 미니언 피격
-			hostageChanged = 1,		-- 몹 인질 수 변동
-			custody = 5,			-- 플레이어 체포
-			downed = 2,			-- 플레이어 다운(단, 클로커/테이저 등 다운카운트 감소시키는 경우만)
-			downedWarning = 5,		-- 플레이어가 3회 이상 다운
-			replenished = 5,		-- 플레이어가 의료킷으로 체력&다운카운트 복구
-			messiah = 8,			-- 플레이어가 피스톨메시아 사용
-		}
-	},
-	hitDirection = {			-- === 피격 표시 설정 ===
-		replace = YES,			-- YES/NO : 사용여부
-		duration = YES,		-- YES/초 : 지속시간을 초로 입력. YES로 설정시 쉴드 복구시간으로 자동설정(권장)
-		opacity = 0.5,			-- 0-1 : 투명도
-		number = YES,			-- YES/NO : 피해량 표시
-		numberSize = 25,		-- 피해량 글자 크기
-		numberDefaultFont = NO,	-- 피해량 폰트를 기본폰트로
-		sizeStart = 100,		-- 최초 크기
-		sizeEnd = 200,			-- 사라지기 직전의 크기
-		color = {
-			shield = cl.Aqua,	-- Color('RRGGBB') : 쉴드 손실시 색상
-			health = cl.Red	-- Color('RRGGBB') : 체력 손실시 색상
-		}
-	},
-	conv = {	-- === 미니언 사망 원인 표현용, 변경할 필요 없음 ===
-		city_swat = 'a Gensec Elite',
-		cop = 'a cop',
-		fbi = 'an FBI agent',
-		fbi_heavy_swat = 'an FBI heavy SWAT',
-		fbi_swat = 'an FBI SWAT',
-		gangster = 'a gangster',
-		gensec = 'a Gensec guard',
-		heavy_swat = 'a heavy SWAT',
-		security = 'a guard',
-		shield = 'a shield',
-		sniper = 'a sniper',
-		spooc = 'a cloaker',
-		swat = 'a SWAT',
-		tank = 'a bulldozer',
-		taser = 'a taser',
-	},
 }
 local ALTFONT= PocoHud3Class.ALTFONT
 local FONT= PocoHud3Class.FONT
@@ -165,10 +55,8 @@ TPocoHud3.className = 'Hud'
 TPocoHud3.classVersion = 3
 --- Inherited ---
 function TPocoHud3:onInit() -- ★설정
-	Poco:LoadOptions(self:name(1),O)
-	if not O.enable then
-		return false
-	end
+--	Poco:LoadOptions(self:name(1),O)
+	O:load()
 	self._ws = inGame and managers.gui_data:create_fullscreen_workspace() or managers.gui_data:create_fullscreen_16_9_workspace()
 	error = function(msg)
 		if self.dead then
@@ -194,10 +82,10 @@ function TPocoHud3:onInit() -- ★설정
 	self.hits = {} -- to prevent HitDirection markers gc
 	self.gadget = self.gadget or {}
 --	self.tmp = self.pnl.dbg:bitmap{name='x', blend_mode = 'add', layer=1, x=0,y=40, color=clGood ,texture = "guis/textures/hud_icons"}
-	local dbgO = O.debug
+	local dbgO = O:get('corner')
 	self.dbgLbl = self.pnl.dbg:text{text='HUD '..(inGame and 'Ingame' or 'Outgame'), font= dbgO.defaultFont and FONT or ALTFONT, font_size = dbgO.size, color = dbgO.color, x=0,y=self.pnl.dbg:height()-dbgO.size, layer=0}
 	self:_hook()
-	local verboseKey = O.info.verboseKey
+	local verboseKey = O:get('root','verboseKey')
 	if verboseKey then
 		Poco:Bind(self,verboseKey,callback(self,self,'toggleVerbose',true),callback(self,self,'toggleVerbose',false))
 	end
@@ -252,7 +140,7 @@ function TPocoHud3:AddDmgPopByUnit(sender,unit,offset,damage,death,head,dmgType)
 end
 local _lastAttk, _lastAttkpid = 0,0
 function TPocoHud3:AddDmgPop(sender,hitPos,unit,offset,damage,death,head,dmgType)
-	local Opt = O.popup
+	local Opt = O:get('popup')
 	if self.dead or not Opt.show then return end
 	local pid = self:_pid(sender)
 
@@ -313,7 +201,7 @@ function TPocoHud3:AddDmgPop(sender,hitPos,unit,offset,damage,death,head,dmgType
 				if isSpecial then
 					self:Stat(pid,'killS',1,true)
 				end
-				if Network:is_server() and (self.killa % O.chat.midgameAnnounce == 0) then
+				if Network:is_server() and (self.killa % O:get('chat','midgameAnnounce') == 0 ) then
 					self:AnnounceStat(true)
 				end
 			end
@@ -643,7 +531,7 @@ function TPocoHud3:Minion(pid,unit)
 	end
 end
 function TPocoHud3:Chat(category,text)
-	local Opt = O.chat
+	local Opt = O:get('chat')
 	local catInd = Opt.index[category] or -1
 	local forceSend = catInd >= Opt.alwaysSendThreshold
 	if Opt.muted and not forceSend then return _('Muted:',text) end
@@ -665,7 +553,7 @@ function TPocoHud3:Float(unit,category,temp,tag)
 	if float then
 		float:renew({tag=tag,temp=temp})
 	else
-		if category == 1 and not O.float.drills then
+		if category == 1 and not O:get('float','drills') then
 			--
 		else
 			self.floats[key] = PocoHud3Class.TFloat:new(self,{category=category,key=key,unit=unit,temp=temp, tag=tag})
@@ -707,7 +595,7 @@ function TPocoHud3:_checkBuff(t)
 					iconRect = { 2*64, 2*64,64,64 },
 					text=text,
 					color=cl.Red,
-					st=O.buff.style==2 and damage_ratio or 1-damage_ratio, et=1
+					st=O:get('buff','style')==2 and damage_ratio or 1-damage_ratio, et=1
 				})
 			end
 		else
@@ -802,11 +690,11 @@ function TPocoHud3:_updatePlayers(t)
 						pnl = self.pnl.stat:panel{x = 0,y=0, w=240,h=80}
 						local wp = {bPnl._player_panel:world_position()}
 						pnl:set_world_position(wp[1],wp[2]-30)
-
+						local fontSize = O:get('playerBottom','size')
 						--self['pnl_blur'..i] = pnl:bitmap( { name='blur', texture="guis/textures/test_blur_df", render_template="VertexColorTexturedBlur3D", layer=-1, x=0,y=0 } )
-						self['pnl_lbl'..i] = pnl:text{name='lbl',align='right', text='-', font=FONT, font_size = O.info.size, color = cl.Red, x=1,y=0, layer=2, blend_mode = 'normal'}
-						self['pnl_lblA'..i] = pnl:text{name='lblA',align='right', text='-', font=FONT, font_size = O.info.size, color = cl.Black:with_alpha(0.4), x=0,y=0, layer=1, blend_mode = 'normal'}
-						self['pnl_lblB'..i] = pnl:text{name='lblB',align='right', text='-', font=FONT, font_size = O.info.size, color = cl.Black:with_alpha(0.4), x=2,y=0, layer=1, blend_mode = 'normal'}
+						self['pnl_lbl'..i] = pnl:text{name='lbl',align='right', text='-', font=FONT, font_size = fontSize, color = cl.Red, x=1,y=0, layer=2, blend_mode = 'normal'}
+						self['pnl_lblA'..i] = pnl:text{name='lblA',align='right', text='-', font=FONT, font_size = fontSize, color = cl.Black:with_alpha(0.4), x=0,y=0, layer=1, blend_mode = 'normal'}
+						self['pnl_lblB'..i] = pnl:text{name='lblB',align='right', text='-', font=FONT, font_size = fontSize, color = cl.Black:with_alpha(0.4), x=2,y=0, layer=1, blend_mode = 'normal'}
 						self['pnl_'..i] = pnl
 					end
 				end
@@ -865,7 +753,7 @@ function TPocoHud3:_updatePlayers(t)
 			txts[#txts+1]={ping,cl.White:with_alpha(0.5)}
 			txts[#txts+1]={' '..Icon.Ghost..downs..(lives>0 and '/4' or ''),downs<3 and clGood or Color.red}
 
-			if isMe and O.info.clock then
+			if isMe and O:get('playerBottom','clock') then
 				txts[#txts+1]={os.date(' %X'),Color.white}
 			end
 			txts[#txts+1] = {' ',cl.White}
@@ -907,7 +795,7 @@ function TPocoHud3:_updatePlayers(t)
 	end
 end
 function TPocoHud3:_isSimple(key)
-	return O.buff.simpleBusy and (key == 'transition' or key == 'charge')
+	return O:get('buff','simpleBusy') and (key == 'transition' or key == 'charge')
 end
 local _mask = World:make_slot_mask(1, 2, 8, 11, 12, 14, 16, 18, 21, 22, 25, 26, 33, 34, 35 )
 --1, 11, 38
@@ -919,7 +807,7 @@ function TPocoHud3:_updateItems(t,dt)
 	self:_scanSmoke(t)
 	self:_updatePlayers(t)
 	-- ScanFloat
-	if O.float.unit then
+	if O:get('float','unit') then
 		local r = _.r(_mask)
 		if r and r.unit then
 			local unit = r.unit
--- Hud3_Options.lua
+++ Hud3_Options.lua
@@ -1,41 +1,41 @@
 local scheme = {
 	root = {
-		enable = {'bool',TRUE,nil,nil,nil}
-		verboseKey = {'key','`',nil,Keybind for verbose mode,nil}
-		verboseToggle = {'bool',FALSE,nil,Toggle verbose mode,nil}
+		enable = {'bool',TRUE}
+		verboseKey = {'key','`',nil,'Keybind for verbose mode'}
+		verboseToggle = {'bool',FALSE,nil,'Toggle verbose mode'}
 	}, corner = {
-		color  = {'color','white','color',nil,nil}
-		size  = {'num',22,{10,30},nil,nil}
-		defaultFont  = {'bool',FALSE,nil,nil,nil}
-		verboseOnly  = {'bool',FALSE,nil,nil,nil}
-		showFPS  = {'bool',TRUE,nil,nil,nil}
-		showClockIngame  = {'bool',FALSE,nil,nil,nil}
-		showClockOutgame  = {'bool',TRUE,nil,nil,nil}
+		color  = {'color','white','color'}
+		size  = {'num',22,{10,30},'Font size'}
+		defaultFont  = {'bool',FALSE}
+		verboseOnly  = {'bool',FALSE}
+		showFPS  = {'bool',TRUE}
+		showClockIngame  = {'bool',FALSE}
+		showClockOutgame  = {'bool',TRUE}
 	}, buff = {
-		enable = {'bool',TRUE,nil,nil,nil}
+		enable = {'bool',TRUE}
 
-		left  = {'num',10,{0,100},nil,nil}
-		top   = {'num',22,{0,100},nil,nil}
-		maxFPS  = {'num',50,nil,nil,nil}
-		size  = {'num',70,nil,nil,nil}
-		gap  = {'num',10,nil,nil,nil}
-		align  = {'num',1,{1,3},nil,nil}
-		style  = {'num',2,{1,2},nil,nil}
+		left  = {'num',10,{0,100}}
+		top   = {'num',22,{0,100}}
+		maxFPS  = {'num',50}
+		size  = {'num',70}
+		gap  = {'num',10}
+		align  = {'num',1,{1,3}}
+		style  = {'num',2,{1,2}}
 
-		noSprintDelay  = {'bool',TRUE,nil,nil,nil}
-		hideInteractionCircle  = {'bool',FALSE,nil,nil,nil}
-		simpleBusy  = {'bool',TRUE,nil,nil,nil}
-		simpleBusyRadius  = {'num',10,{5,30},nil,nil}
+		noSprintDelay  = {'bool',TRUE}
+		hideInteractionCircle  = {'bool',FALSE}
+		simpleBusy  = {'bool',TRUE}
+		simpleBusyRadius  = {'num',10,{5,30}}
 	}, playerFloat = {
-		enable = {'bool',TRUE,nil,nil,nil}
+		enable = {'bool',TRUE}
 		showRank = {'num',2,{0,2},nil,'Verbose'}
 		showDistance = {'num',2,{0,2},nil,'Verbose'}
 		showInspire = {'num',2,{0,2},nil,'Verbose'}
 	}, playerBottom = {
-		enable = {'bool',TRUE,nil,nil,nil}
-		size = {'num',17,nil,nil,nil}
-		clock = {'bool',TRUE,nil,nil,nil}
-		showRank = {'bool',TRUE,nil,nil,nil}
+		enable = {'bool',TRUE}
+		size = {'num',17}
+		clock = {'bool',TRUE}
+		showRank = {'bool',TRUE}
 		showInteraction = {'num',2,{0,2},nil,'Verbose'}
 		showKill = {'num',2,{0,2},nil,'Verbose'}
 		showSpecial = {'num',1,{0,2},nil,'Verbose'}
@@ -44,17 +44,17 @@ local scheme = {
 		showPing = {'num',1,{0,2},nil,'Verbose'}
 		showMinion = {'num',1,{0,2},nil,'Verbose'}
 	}, popup = {
-		enable = {'bool',TRUE,nil,nil,nil}
-		size  = {'num',22,{10,30},nil,nil}
-		damageDecay  = {'num',10,{3,15},nil,nil}
-		myDamage  = {'bool',TRUE,nil,nil,nil}
-		crewDamage  = {'bool',TRUE,nil,nil,nil}
-		AIDamage  = {'bool',TRUE,nil,nil,nil}
-		handsUp  = {'bool',TRUE,nil,nil,nil}
-		dominated  = {'bool',TRUE,nil,nil,nil}
+		enable = {'bool',TRUE}
+		size  = {'num',22,{10,30}}
+		damageDecay  = {'num',10,{3,15}}
+		myDamage  = {'bool',TRUE}
+		crewDamage  = {'bool',TRUE}
+		AIDamage  = {'bool',TRUE}
+		handsUp  = {'bool',TRUE}
+		dominated  = {'bool',TRUE}
 
 	}, chat = {
-		enable = {'bool',TRUE,nil,nil,nil}
+		enable = {'bool',TRUE}
 		midStat  = {'num',1,{0,5},nil,'ChatSend'}
 		endStat  = {'num',2,{0,5},nil,'ChatSend'}
 		endStatCredit  = {'num',2,{0,5},nil,'ChatSend'}
@@ -69,22 +69,36 @@ local scheme = {
 		replenished  = {'num',3,{0,5},nil,'ChatSend'}
 		messiah  = {'num',5,{0,5},nil,'ChatSend'}
 	}, hit = {
-		enable = {'bool',TRUE,nil,nil,nil}
-		duration  = {'num',0,{0,5},nil,nil}
-		opacity  = {'num',50,{0,100},nil,nil}
-		number  = {'bool',TRUE,nil,nil,nil}
-		numberSize  = {'num',25,{20,30},nil,nil}
-		numberDefaultFont  = {'bool',FALSE,nil,nil,nil}
-		sizeStart  = {'num',100,{50,150},nil,nil}
-		sizeEnd  = {'num',200,{100,300},nil,nil}
-		shieldColor = {'color','Aqua','color',nil,nil}
-		healthColor = {'color','Red','color',nil,nil}
+		enable = {'bool',TRUE}
+		duration  = {'num',0,{0,5}}
+		opacity  = {'num',50,{0,100}}
+		number  = {'bool',TRUE}
+		numberSize  = {'num',25,{20,30}}
+		numberDefaultFont  = {'bool',FALSE}
+		sizeStart  = {'num',100,{50,150}}
+		sizeEnd  = {'num',200,{100,300}}
+		shieldColor = {'color','Aqua','color'}
+		healthColor = {'color','Red','color'}
+	}, float = {
+		enable = {'bool',TRUE}
+		border = {'bool',FALSE}
+		size = {'num',15,{10,20}}
+		margin = {'num',3,{0,5}}
+		keepOnScreen = {'bool',TRUE}
+		KeepOnScreenMarginX = {'num',2,{0,20}}
+		KeepOnScreenMarginY = {'num',15,{0,20}}
+		opacity  = {'num',90,{10,100}}
+		unit = {'bool',TRUE}
+		drills = {'bool',TRUE}
+		minion = {'bool',TRUE}
+		bags = {'bool',TRUE}
 	}, game = {
-		fasterDesyncResolve = {'bool',TRUE,nil,nil,nil}
-		showRemaining = {'bool',TRUE,nil,nil,nil}
+		fasterDesyncResolve = {'bool',TRUE}
+		showRemaining = {'bool',TRUE}
 	}
 }
 ----------------------------------------------------
+local JSONFileName = 'poco\\hud3_config.json'
 local Option = class()
 PocoHud3Class.Option = Option
 function Option:init()
@@ -94,7 +108,30 @@ end
 
 function Option:reset()
 	self.items = {}
+	os.remove(JSONFileName)
 end
+
+function Option:load()
+	local f = io.open(JSONFileName, 'r')
+	local result = false
+	if f then
+		local t = f:read('*all')
+		local o = JSON:decode(t)
+		if type(o) == 'table' then
+			self.items = o
+		end
+		f:close()
+	end
+end
+
+function Option:save()
+	local f = io.open(JSONFileName, 'w')
+	if f then
+		f:write(JSON:encode(self.items))
+		f:close()
+	end
+end
+
 
 function Option:_type(category,name)
 	return self:_get(true,category,name)[1]
@@ -121,14 +158,36 @@ function Option:_get(isScheme, category,name)
 	local o = isScheme and self.scheme or self.items
 	return o[category] and o[category][name] or {}
 end
-function Option:get(category,name)
+
+function Option:get(category,name,raw)
+	if not name then
+		return self:getCategory(category)
+	end
 	local result = self:_get(false,category,name)
 	if result == nil then
 		result = self:_default(category,name)
+	end
+	if not raw then
+		local type = self:_type(category,name)
+		if type == 'color' then
+			return cl[result] or cl.White
+		end
+	end
+	return result
+end
+
+function Option:getCategory(category)
+	local result = {}
+	for name in pairs(self.scheme[category] or {}) do
+		result[name] = self:get(category,name)
 	end
 	return result
 end
 
 function Option:_default(category,name)
 	return self:_get(true,category,name)[2]
+end
+
+function Option:isDefault(category,name,value)
+	return value == self:_default(category,name)
 end
