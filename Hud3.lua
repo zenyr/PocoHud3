c635b4748f558ca06819c4592144a83143a1059e
Zenyr <zenyr@zenyr.com>
2014-09-17 15:49:23 +0900

    test commit2

--- Hud3.lua
+++ Hud3.lua
@@ -6,8 +6,8 @@ feel free to ask me through my mail: zenyr@zenyr.com. But please understand that
 
 
 local _ = UNDERSCORE
-local REV = 234
-local TAG = '0.192 hotfix 4 (g762c4f4)'
+local REV = 236
+local TAG = '0.192 hotfix 6 (gca47c36)'
 local inGame = CopDamage ~= nil
 local inGameDeep
 local me
