e3f0a79068670c92d03e691f3d67dfe214b404e3
zenyr <zenyr@zenyr.com>
2015-01-03 16:29:44 +0900

    i18n: Added Svenska(Swedish) i18n: Updated German

--- Hud3.lua
+++ Hud3.lua
@@ -6,8 +6,8 @@ feel free to ask me through my mail: zenyr@zenyr.com. But please understand that
 
 -- Note: Due to quirky PreCommit hook, revision number would *appear to* be 1 revision older than released luac files.
 local _ = UNDERSCORE
-local REV = 303
-local TAG = 'v0.23 hotfix 7 (1d90187)'
+local REV = 304
+local TAG = 'v0.23 hotfix 8 (4c34df2)'
 local inGame = CopDamage ~= nil
 local inGameDeep
 local me
