f5b118285b0940fea91b92df884ab28eeb70a761
zenyr <zenyr@zenyr.com>
2015-03-08 14:51:27 +0900

    * i18n: Updated IT translation (10 lines)

--- Hud3.lua
+++ Hud3.lua
@@ -6,8 +6,8 @@ feel free to ask me through my mail: zenyr@zenyr.com. But please understand that
 
 -- Note: Due to quirky PreCommit hook, revision number would *appear to* be 1 revision older than released luac files.
 local _ = UNDERSCORE
-local REV = 331
-local TAG = '0.26'
+local REV = 332
+local TAG = '0.26 hotfix 1 (89b32d6)'
 local inGame = CopDamage ~= nil
 local inGameDeep
 local me
