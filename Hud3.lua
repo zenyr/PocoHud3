8af4d04525accb4501ee1c8edbc2d91f07d3f6cb
zenyr <zenyr@zenyr.com>
2015-03-28 17:33:04 +0900

    i18n: Fixed typo i18n: ES(+19), IT(+16), RU(+30), SV(+87) Updated

--- Hud3.lua
+++ Hud3.lua
@@ -6,8 +6,8 @@ feel free to ask me through my mail: zenyr(at)zenyr.com. But please understand t
 
 -- Note: Due to quirky PreCommit hook, revision number would *appear to* be 1 revision before than "released" luac files.
 local _ = UNDERSCORE
-local REV = 347
-local TAG = '0.26 hotfix 16 (32c5729)'
+local REV = 348
+local TAG = '0.261'
 local inGame = CopDamage ~= nil
 local inGameDeep
 local me
