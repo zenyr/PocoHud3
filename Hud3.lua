6ebc5c3387cf97761177b6dbdba28e8a2e1bb3b0
Zenyr <zenyr@zenyr.com>
2015-08-31 20:39:07 +0900

    Apply tag

--- Hud3.lua
+++ Hud3.lua
@@ -5,8 +5,8 @@ feel free to ask me through my mail: zenyr(at)zenyr.com. But please understand t
 ]]
 -- Note: Due to quirky PreCommit hook, revision number would *appear to* be 1 revision before than "released" luac files.
 local _ = UNDERSCORE
-local REV = 381
-local TAG = '0.29 hotfix 5 (73a8349)'
+local REV = 383
+local TAG = '0.29 hotfix 6'
 local inGame = CopDamage ~= nil
 local inGameDeep
 local me
